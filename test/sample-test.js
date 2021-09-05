adc69dad3177c7a76da1d4ec55d5280a01acd611
Pegah <43738935+pegahfallah@users.noreply.github.com>
2021-09-05 12:03:58 -0400

    testing token

--- test/sample-test.js
+++ test/sample-test.js
@@ -2,19 +2,31 @@ const { expect } = require("chai");
 //library for testing in JS
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
-
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+describe("Token", function () {
+  let token;
+  const name = "Pegah Token";
+  const symbol = "PT";
+  beforeEach(async () => {
+    const Token = await ethers.getContractFactory("Token");
+    token = await Token.deploy(name, symbol);
+  });
+  it("Deploy with name and symbol", async function () {
+    await token.deployed();
+    expect(await token.name()).to.equal(name);
+    expect(await token.symbol()).to.equal(symbol);
 
     // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
+  it("should give the deployer total supply", async function () {
+    await token.deployed();
+    const owner = await token.owner();
+    const totalSupply = await token.totalSupply();
+    console.log(owner, totalSupply); //random address hardhat creates
+    expect(await token.balances(owner)).to.equal(totalSupply);
+  });
+  // it("sets the owner to the deployer", async function () {});
+  // describe("function", function () {
+  //   it("should revert if the sender does not have sufficient funds", async function () {});
+  //   it("should decrease sender balance and increase recipient balance", async function () {});
+  // });
 });
